ee1f5840a0d2c672d5eff44533ebb9f26e354e05
Abdullah Michael Moulay <114344532+Michael971207@users.noreply.github.com>
2023-01-30 11:04:57 +0100

    Create Docs.java

---
+++ Docs.java
@@ -0,0 +1,48 @@
+basic API reference and a user guide:
+
+markdown
+Copy code
+docs/
+├── API Reference/
+│   └── Example.md
+└── User Guide/
+    └── Getting Started.md
+Here is the content of API Reference/Example.md:
+
+javascript
+Copy code
+# Example API Reference
+
+## `Example` class
+
+### `main` method
+
+The `main` method is the entry point of the Example class and prints a message to the console.
+
+public static void main(String[] args) {
+System.out.println("Hello from Example class!");
+}
+
+css
+Copy code
+
+This file provides a description of the `Example` class, including details on its `main` method. The API reference is a technical reference for developers, providing information on how to use the code in your project.
+
+Here is the content of `User Guide/Getting Started.md`:
+
+Getting Started
+This guide will help you get started using the Example class in your project.
+
+Prerequisites
+Java development environment
+Installing
+To use the Example class in your project, simply add the Example.java file to your project's src directory.
+
+Using the Example class
+The Example class can be executed from the command line using the following command:
+
+Copy code
+java com.example.Example
+This will print the message "Hello from Example class!" to the console.
+
+This file provides a user guide for end-users, providing instructions on how to install and use the Example class in their project. The docs directory is a great place to include additional documentation that will help others understand your code and how to use it
